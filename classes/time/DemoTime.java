45fcefbe3542767220b6d7f693ed48b77496b552
Enzo Morgan <112134107+enzomorgan@users.noreply.github.com>
2025-03-21 19:03:01 -0300

    Update DemoTime.java

--- classes/time/DemoTime.java
+++ classes/time/DemoTime.java
@@ -2,6 +2,6 @@ public class DemoTime {
     public static void main(String[] args) {
         Time t1 = new Time ("Flamengo", 11, "Jorge Jesus", "Vermelho e Preto");
 
-        t1.mostraTime();
+        System.out.println(t1);
     }
 }
